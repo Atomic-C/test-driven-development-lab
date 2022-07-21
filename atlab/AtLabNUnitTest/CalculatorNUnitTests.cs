587fcf380855fef2ab1f0210c01689ba17e6322b
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-07-21 00:01:29 +0000

    Added first NUnit test for Sum method.

---
+++ atlab/AtLabNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atlab
+{
+    [TestFixture]
+    class CalculatorNUnitTests
+    {
+        [Test]
+        public void Sum_InputTwoInt_GetCorrectAdition()
+        {
+            // Arranje
+            Calculator calculator = new Calculator();
+            // Act
+            int result = calculator.Sum(2, 2);
+            // Assert
+            //Assert.IsTrue(result == 4);
+            Assert.AreEqual(4, result);
+        }
+    }
+}
