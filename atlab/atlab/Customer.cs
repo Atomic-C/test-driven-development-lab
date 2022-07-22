c2a50a88d3301735181a968fc33494c9fcb9e3d8
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-07-22 17:58:28 +0000

    Added Customer class to test strings with NUnit tests.

---
+++ atlab/atlab/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atlab
+{
+    public class Customer
+    {
+        public string CombineCustomerName(string firstName, string secondName)
+        {
+            return $@"{firstName} {secondName}";
+        }
+    }
+}
