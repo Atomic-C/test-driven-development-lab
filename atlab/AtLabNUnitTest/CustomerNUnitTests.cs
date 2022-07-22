5d6f202e1e66223d8cb08427ae99db6b9d946556
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-07-22 19:07:43 +0000

    Added some string Assert Helper Methods and implemented them on CombineCustomerName NUnit Test.

--- atlab/AtLabNUnitTest/CustomerNUnitTests.cs
+++ atlab/AtLabNUnitTest/CustomerNUnitTests.cs
@@ -19,6 +19,19 @@ namespace atlab
             string fullName = customer.CombineCustomerName("Pedro", "Sousa"); // Stores fullName
             // Assert
             Assert.That(fullName, Is.EqualTo("Pedro Sousa")); // Checks if fullName Is.EquLTo(the expected full name!)
+            Assert.AreEqual(fullName, "Pedro Sousa"); // This is an alternative to line 21.
+
+            // Below are some Assert Helper Methods that can be helpful with strings:
+
+            Assert.That(fullName, Does.Contain("Pedro"));
+            Assert.That(fullName, Does.StartWith("P"));
+            Assert.That(fullName, Does.EndWith("a"));
+
+
+            Assert.That(fullName, Does.Contain("pedro").IgnoreCase); // We can ignore case if needed.
+
+            // Below is a regular expression that can be used with strings, making sure strings start with this pattern: 
+            Assert.That(fullName, Does.Match("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]")); 
         }
     }
 }
