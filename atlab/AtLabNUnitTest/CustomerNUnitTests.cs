19620945f9fbb2542e126238c9b0e077451d16b5
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-07-22 18:34:11 +0000

    Added new NUnit test for Customer class to check if CombineCustomerName returns fullName.

---
+++ atlab/AtLabNUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atlab
+{
+    [TestFixture]
+   public  class CustomerNUnitTests
+    {
+        [Test]
+        public void CombineCustomerName_InputFirstAndLastName_ReturnFfullName()
+        {
+            // Arrange
+            Customer customer = new Customer();
+            // Action
+            string fullName = customer.CombineCustomerName("Pedro", "Sousa"); // Stores fullName
+            // Assert
+            Assert.That(fullName, Is.EqualTo("Pedro Sousa")); // Checks if fullName Is.EquLTo(the expected full name!)
+        }
+    }
+}
