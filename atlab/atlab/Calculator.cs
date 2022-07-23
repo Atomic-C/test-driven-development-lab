dbdbea2bcba04a7862cd8f245f98aa1297c0a9c5
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-07-23 17:59:25 +0000

    Added documentation to the Calculator Methods.

--- atlab/atlab/Calculator.cs
+++ atlab/atlab/Calculator.cs
@@ -7,35 +7,53 @@ namespace atlab
     {
         public List<int> NumberRange = new List<int>();
 
+        /// <summary>
+        /// Takes two integer values
+        /// </summary>
+        /// <param name="a">First integer</param>
+        /// <param name="b">Second integer</param>
+        /// <returns>The Sum between a and b</returns>
         public int Sum(int a, int b) 
         {
             return a + b;
         }
 
+        /// <summary>
+        /// Takes two integer values
+        /// </summary>
+        /// <param name="a">First integer</param>
+        /// <param name="b">Second integer</param>
+        /// <returns>The subtraction between a and b</returns>
         public int Subtract(int a, int b)
         {
             return a - b;
         }
         
+        /// <summary>
+        /// Takes two double values
+        /// </summary>
+        /// <param name="a">First double</param>
+        /// <param name="b">Second double</param>
+        /// <returns>The Sum between a and b</returns>
         public double SumDouble(double a, double b) 
         {
             return a + b;
         }
-
+        /// <summary>
+        /// Takes a integer as input
+        /// </summary>
+        /// <param name="a">The number to be evaluated</param>
+        /// <returns>true if Odd and false if Even</returns>
         public bool IsOddNumber(int a)
         {
             return a % 2 != 0;
         }
-
-
-
-
         /// <summary>
         /// Takes a min and max input values.
         /// </summary>
         /// <param name="min">Minimum range</param>
         /// <param name="max">Max range</param>
-        /// <returns>The range of Odd Numbers between inputs.</returns>
+        /// <returns>The range of Odd Numbers between min and max input</returns>
         public List<int> GetOddRange(int min, int max)
         {
             NumberRange.Clear();
