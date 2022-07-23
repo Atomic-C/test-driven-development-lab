5566b2db76f84bfa3e2bdfb69dd10e36bb62f36b
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-07-23 17:37:57 +0000

    Added method that returns the range of odd umbers between two integer inputs.

--- atlab/atlab/Calculator.cs
+++ atlab/atlab/Calculator.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace atlab
 {
     public class Calculator
     {
+        public List<int> NumberRange = new List<int>();
+
         public int Sum(int a, int b) 
         {
             return a + b;
@@ -22,6 +25,28 @@ namespace atlab
         public bool IsOddNumber(int a)
         {
             return a % 2 != 0;
+        }
+
+
+
+
+        /// <summary>
+        /// Takes a min and max input values.
+        /// </summary>
+        /// <param name="min">Minimum range</param>
+        /// <param name="max">Max range</param>
+        /// <returns>The range of Odd Numbers between inputs.</returns>
+        public List<int> GetOddRange(int min, int max)
+        {
+            NumberRange.Clear();
+            for (int i = min; i <= max; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    NumberRange.Add(i);
+                }
+            }
+            return NumberRange;
         }
     }
 }
