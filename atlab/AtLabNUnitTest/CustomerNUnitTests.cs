4eaee309cde1b49ab2a8f0641aa52a0f6bf94887
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-07-26 00:21:42 +0000

    Added Assert Multiple to enable multiple errors to show up as opposed to only the first one.

--- atlab/AtLabNUnitTest/CustomerNUnitTests.cs
+++ atlab/AtLabNUnitTest/CustomerNUnitTests.cs
@@ -24,21 +24,25 @@ namespace atlab
             //Customer customer = new Customer();
             // Action
             string fullName = customer.CombineCustomerName("Pedro", "Sousa"); // Stores fullName
-            // Assert
-            Assert.That(fullName, Is.EqualTo("Hello, Pedro Sousa")); // Checks if fullName Is.EquLTo(the expected full name!)
-            Assert.AreEqual(fullName, "Hello, Pedro Sousa"); // This is an alternative to line 21.
+                                                                              // Assert
 
-            // Below are some Assert Helper Methods that can be helpful with strings:
+            Assert.Multiple(() => // This enables multiple errors to show up, that won't without this Multiple method.
+            {
+                Assert.That(fullName, Is.EqualTo("Hello, Pedro Sousa")); // Checks if fullName Is.EquLTo(the expected full name!)
+                Assert.AreEqual(fullName, "Hello, Pedro Sousa"); // This is an alternative to line 21.
 
-            Assert.That(fullName, Does.Contain("Pedro"));
-            Assert.That(fullName, Does.StartWith("H"));
-            Assert.That(fullName, Does.EndWith("a"));
+                 // Below are some Assert Helper Methods that can be helpful with strings:
+
+                 Assert.That(fullName, Does.Contain("Pedro"));
+                 Assert.That(fullName, Does.StartWith("H"));
+                 Assert.That(fullName, Does.EndWith("a"));
 
 
-            Assert.That(fullName, Does.Contain("pedro").IgnoreCase); // We can ignore case if needed.
+                 Assert.That(fullName, Does.Contain("pedro").IgnoreCase); // We can ignore case if needed.
 
-            // Below is a regular expression that can be used with strings, making sure strings start with this pattern: 
-            Assert.That(fullName, Does.Match("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]")); 
+                 // Below is a regular expression that can be used with strings, making sure strings start with this pattern: 
+                 Assert.That(fullName, Does.Match("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]")); 
+            });
         }
 
         [Test]
