17fa6d2f85a038c1e9561fc18326ca37ddc81e2d
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-07-27 15:16:56 +0000

    Added GradingCalculator, which provides grade based on score and attendance. Added NUnit tests.

---
+++ atlab/AtLabNUnitTest/GradingCalculatorNUnitTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atlab
+{
+    [TestFixture]
+    public class GradingCalculatorNUnitTests
+    {
+        public GradingCalculator gradingCalculator;
+        [SetUp]
+        public void Setup() // This works like a constructor and exists to replace Arrange object instantiation.
+        {
+            gradingCalculator = new GradingCalculator();
+        }
+
+        [Test]
+        public void Score95AndAttendance90_ReturnA()
+        {
+            {
+                // Arrange
+                //GradingCalculator Calculator = new GradingCalculator(); // Because we instantiate on Setup()
+                // Act
+                int scoreResult = gradingCalculator.Score = 95;
+                int attendanceResult = gradingCalculator.AttendancePercentage = 90;
+                string gradeResult = gradingCalculator.GetGrade();
+                // Assert
+                Assert.That(gradeResult, Is.EqualTo("A"));
+            }
+
+        }
+
+        [Test]
+        public void Score85AndAttendance90_ReturbB()
+        {
+            // Arrange
+            //GradingCalculator Calculator = new GradingCalculator(); // Because we instantiate on Setup()
+            // Act
+            int scoreResult = gradingCalculator.Score = 85;
+            int attendanceResult = gradingCalculator.AttendancePercentage = 90;
+            string gradeResult = gradingCalculator.GetGrade();
+            // Assert
+            Assert.That(gradeResult, Is.EqualTo("B"));
+        }
+
+        [Test]
+        public void Score65AndAttendance90_ReturnC()
+        {
+            // Arrange
+            //GradingCalculator Calculator = new GradingCalculator(); // Because we instantiate on Setup()
+            // Act
+            int scoreResult = gradingCalculator.Score = 65;
+            int attendanceResult = gradingCalculator.AttendancePercentage = 90;
+            string gradeResult = gradingCalculator.GetGrade();
+            // Assert
+            Assert.That(gradeResult, Is.EqualTo("C"));
+        }
+
+        [Test]
+        public void Score95AndAttendance65_ReturnB()
+        {
+            // Arrange
+            //GradingCalculator Calculator = new GradingCalculator(); // Because we instantiate on Setup()
+            // Act
+            int scoreResult = gradingCalculator.Score = 95;
+            int attendanceResult = gradingCalculator.AttendancePercentage = 65;
+            string gradeResult = gradingCalculator.GetGrade();
+            // Assert
+            Assert.That(gradeResult, Is.EqualTo("B"));
+        }
+
+        [Test]
+        [TestCase(95,55)]
+        [TestCase(65,55)]
+        [TestCase(50,90)]
+        public void Score95_65_50_Attendance55_55_90_ReturnF(int score, int attendanceScore)
+        {
+            // Arrange
+            //GradingCalculator Calculator = new GradingCalculator(); // Because we instantiate on Setup()
+            // Act
+            int scoreResult = score;
+            int attendanceResult = attendanceScore;
+            string gradeResult = gradingCalculator.GetGrade();
+            // Assert
+            Assert.That(gradeResult, Is.EqualTo("F"));
+        }
+    }
+}
---
+++ atlab/atlab/GradingCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atlab
+{
+    public class GradingCalculator
+    {
+        public int Score { get; set; }
+        public int AttendancePercentage { get; set; }
+
+        /// <summary>
+        /// Grades studnets according to their Score and AttendancePercentage 
+        /// </summary>
+        /// <returns>Grade</returns>
+        public string GetGrade()
+        {
+            if (Score > 90 && AttendancePercentage > 70)
+                return "A";
+            else
+                if (Score > 80 && AttendancePercentage > 60)
+                return "B";
+            else
+                    if (Score > 60 & AttendancePercentage > 60)
+                return "C";
+            else
+                return "F";
+        }
+    }
+}
