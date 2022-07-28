60133be92bc669084bdf30224b06074ac60473a2
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-07-28 15:38:48 +0000

    Created my first Integration Test since it's testing both BankAccount and LogBook.

---
+++ atlab/AtLabNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atlab
+{
+    [TestFixture]
+    public class BankAccountNUnitTests
+    {
+        private BankAccount bankAccount;
+
+        [SetUp]
+        public void Setup()
+        {
+            bankAccount = new BankAccount(new LogBook());
+        }
+
+        [Test]
+        public void BankDeposit_100USD_ReturnTrue()
+        {
+            var result = bankAccount.Deposit(100);
+            Assert.IsTrue(result);
+            Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+        }
+    }
+}
