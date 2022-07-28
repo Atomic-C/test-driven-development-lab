493a6931494923cbf6c534bbf89ace5870550e6a
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-07-28 16:12:33 +0000

    Added MOQ dependency, Implemented BankDeposit NUnit test with Mocked ILogBook. It's simpler and cleaner than Dummy code.

--- atlab/AtLabNUnitTest/BankAccountNUnitTests.cs
+++ atlab/AtLabNUnitTest/BankAccountNUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,20 +11,37 @@ namespace atlab
     [TestFixture]
     public class BankAccountNUnitTests
     {
-        private BankAccount bankAccount;
+        private BankAccount account;
 
         [SetUp]
         public void Setup()
         {
-            bankAccount = new BankAccount(new DummyLogBook());
+
         }
 
         [Test]
-        public void BankDeposit_100USD_ReturnTrue()
+        public void BankDeposit_DummyLogBook_100USD_ReturnTrue()
         {
+            // Arrange
+            BankAccount bankAccount = new BankAccount(new DummyLogBook());
+            // Act
             var result = bankAccount.Deposit(100);
+            // Assert
             Assert.IsTrue(result);
             Assert.That(bankAccount.GetBalance, Is.EqualTo(100));
+        }        
+        
+        [Test]
+        public void BankDeposit_Mocked_100USD_ReturnTrue()
+        {
+            // Arrange
+            var logMock = new Mock<ILogBook>();
+            BankAccount account = new BankAccount(logMock.Object);
+            // Act
+            var result = account.Deposit(100);
+            // Assert
+            Assert.IsTrue(result);
+            Assert.That(account.GetBalance, Is.EqualTo(100));
         }
     }
 }
